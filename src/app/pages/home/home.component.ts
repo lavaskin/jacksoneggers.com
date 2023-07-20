a7d03f4275f349e642d4e62d499fe16fceefc325
Jackson E <lavaskin@gmail.com>
2023-07-19 18:43:17 -0700

    add fade-in animation to home page on desktop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,6 +17,25 @@ export class HomeComponent implements OnInit {
 
 	ngOnInit(): void { }
 
+	ngAfterViewInit(): void {
+		// Grab all the section tags outside of the first
+		const sectionsCollection = document.getElementsByTagName('section') as HTMLCollectionOf<HTMLElement>;
+		const sections = Array.from(sectionsCollection);
+		
+		// Make them all fade in sequentially
+		function fadeInSections(sections: any, index: number) {
+			if (index >= sections.length) return;
+			
+			const section = sections[index];
+			section.classList.add('slide-fade-in'); // Add the class to trigger the animation after a delay
+			
+			// Start the next animation 250ms after the current one starts
+			setTimeout(() => fadeInSections(sections, index + 1), 250);
+		}
+		  
+		fadeInSections(sections, 1); // Start the fading animation
+	}
+
 	// Finds a project by ID, if it doesn't exist, return the first project
 	private getFeaturedProject(id: number): Project {
 		const project = projectsData.find(project => project.id === id);
