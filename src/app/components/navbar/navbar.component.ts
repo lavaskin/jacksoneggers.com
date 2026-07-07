eb726d1d26d968d29942b6dff2619ddf67926bb6
lavaskin <lavaskin@gmail.com>
2026-07-06 23:37:23 -0500

    properly handle scroll listener

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+const SCROLL_EVENT_KEY = 'scroll';
 
 @Component({
     selector: 'app-navbar',
@@ -9,24 +11,33 @@ import { RouterModule } from '@angular/router';
 		RouterModule
 	],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 	public color: string = 'transparent';
 	public border: string = 'none';
+
+	private _scrollHandler: (() => void) | undefined;
 
 	ngOnInit(): void {
 		const navbar = document.getElementsByTagName('nav')[0];
 
-		// Hook up scroll listener to change navbar styling
-		window.addEventListener('scroll', () => {
+		this._scrollHandler = () => {
 			if (window.scrollY > 10) {
 				navbar.classList.add('scrolled');
 			} else {
 				navbar.classList.remove('scrolled');
 			}
-		});
+		};
+
+		window.addEventListener(SCROLL_EVENT_KEY, this._scrollHandler);
 	}
 
 	public scrollToTop(): void {
 		window.scrollTo({ top: 0 });
+	}
+
+	ngOnDestroy(): void {
+		if (this._scrollHandler) {
+			window.removeEventListener(SCROLL_EVENT_KEY, this._scrollHandler);
+		}
 	}
 }
